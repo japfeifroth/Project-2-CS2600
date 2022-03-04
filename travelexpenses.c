d52458035cc01144fa5cca8f6c909e889763f296
japfeifroth <89482008+japfeifroth@users.noreply.github.com>
2022-03-03 20:51:32 -0800

    main

---
+++ travelexpenses.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "functions.c"
+
+int getDays(int);
+
+int main()
+{
+    int days;
+    days = getDays(days); 
+    printf("Days: %d\n", days);
+    return 0;
+}
