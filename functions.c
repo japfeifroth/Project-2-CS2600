99e357e6304294647cf8d951ced5aabc5cd7453d
japfeifroth <89482008+japfeifroth@users.noreply.github.com>
2022-03-03 21:25:29 -0800

    Update functions.c

--- functions.c
+++ functions.c
@@ -6,9 +6,10 @@ int getDays(int days)
 {
     a: printf("How many days were spent on the trip?\n");
     scanf("%d", &days);
+    getchar();
     while(days < 1)
     {
-        printf("Enter a number greater than 1.");
+        printf("Enter a number greater than 1.\n");
         goto a;
     }
     return days;
